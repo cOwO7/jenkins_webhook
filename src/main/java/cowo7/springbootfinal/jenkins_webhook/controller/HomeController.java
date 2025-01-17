3240fcd2824b5934637b21be041bb20122763fcd
cOwO7 <oclc00@naver.com>
2025-01-17 10:52:02 +0900

    Initial commit

---
+++ src/main/java/cowo7/springbootfinal/jenkins_webhook/controller/HomeController.java
@@ -0,0 +1,13 @@
+package cowo7.springbootfinal.jenkins_webhook.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Hello World";
+    }
+}
